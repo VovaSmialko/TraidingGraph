435e46ea71a9c6bad92cfdf8cece0141cbf23027
vova77 <vova2000smi@gmail.com>
2024-03-31 22:59:50 +0300

    Added Assist Chip

--- app/src/main/java/com/smialko/myapplication/data/ApiService.kt
+++ app/src/main/java/com/smialko/myapplication/data/ApiService.kt
@@ -1,8 +1,12 @@
 package com.smialko.myapplication.data
 
+import com.smialko.myapplication.presentation.TimeFrame
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
-    @GET("/v2/aggs/ticker/AAPL/range/1/hour/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=aJcINckE_iDWnXJhalNs_BsZr0PoUjYM")
-    suspend fun loadBars(): Result
+    @GET("/v2/aggs/ticker/AAPL/range/{timeframe}/2022-01-09/2023-01-09?adjusted=true&sort=desc&limit=50000&apiKey=aJcINckE_iDWnXJhalNs_BsZr0PoUjYM")
+    suspend fun loadBars(
+        @Path("timeframe") timeFrame: String
+    ): Result
 }
